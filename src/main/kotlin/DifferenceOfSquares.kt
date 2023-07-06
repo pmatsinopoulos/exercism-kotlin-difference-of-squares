899394111c94cfd0ab3d1df7f3377941ea3bd02e
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-06 11:58:21 +0300

    Implement using fold

--- src/main/kotlin/DifferenceOfSquares.kt
+++ src/main/kotlin/DifferenceOfSquares.kt
@@ -1,33 +1,18 @@
-import kotlin.math.pow
-
 class Squares(private val maxNatural: Int) {
     fun sumOfSquares(): Int {
-        return sumOfNSquare(maxNatural)
+        return (1..maxNatural).fold(0) { acc, n -> acc + n * n }
     }
 
     fun squareOfSum(): Int {
-        return square(sumOfN(maxNatural))
+        val s = sum()
+        return s * s
     }
 
     fun difference(): Int {
         return squareOfSum() - sumOfSquares()
     }
 
-    private fun sumOfN(n: Int): Int {
-        if (n == 1) {
-            return 1
-        }
-        return sumOfN(n - 1) + n
-    }
-
-    private fun square(n: Int): Int {
-        return n.toDouble().pow(2.0).toInt()
-    }
-
-    private fun sumOfNSquare(n: Int): Int {
-        if (n == 1) {
-            return 1
-        }
-        return sumOfNSquare(n - 1) + square(n)
+    private fun sum(): Int {
+        return (1..maxNatural).fold(0) { acc, n -> acc + n }
     }
 }
